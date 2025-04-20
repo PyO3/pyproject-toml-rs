8e99520d07fb2c247224a66267208ac05c3ed485
Olivier Lacroix <olivier.lacroix@m4x.org>
2025-04-20 21:27:14 +1000

    Add a method to resolve recursion in optional dependencies

--- src/pep735_resolve.rs
+++ src/pep735_resolve.rs
@@ -1,93 +1,23 @@
-use indexmap::IndexMap;
-use pep508_rs::Requirement;
-use thiserror::Error;
+use crate::has_recursion::{HasRecursion, Item, RecursionItem};
 
 use crate::{DependencyGroupSpecifier, DependencyGroups};
 
-#[derive(Debug, Error)]
-pub enum Pep735Error {
-    #[error("Failed to find group `{0}` included by `{1}`")]
-    GroupNotFound(String, String),
-    #[error("Detected a cycle in `dependency-groups`: {0}")]
-    DependencyGroupCycle(Cycle),
-}
+impl HasRecursion<DependencyGroupSpecifier> for DependencyGroups {}
 
-/// A cycle in the `dependency-groups` table.
-#[derive(Debug)]
-pub struct Cycle(Vec<String>);
-
-/// Display a cycle, e.g., `a -> b -> c -> a`.
-impl std::fmt::Display for Cycle {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let [first, rest @ ..] = self.0.as_slice() else {
-            return Ok(());
-        };
-        write!(f, "`{first}`")?;
-        for group in rest {
-            write!(f, " -> `{group}`")?;
+impl RecursionItem for DependencyGroupSpecifier {
+    fn parse(&self, _name: Option<&str>) -> Item {
+        match self {
+            DependencyGroupSpecifier::String(requirement) => Item::Requirement(requirement.clone()),
+            DependencyGroupSpecifier::Table {
+                include_group: group,
+            } => Item::Groups(vec![group]),
         }
-        write!(f, " -> `{first}`")?;
-        Ok(())
     }
-}
-
-impl DependencyGroups {
-    /// Resolve dependency groups (which may contain references to other groups) into concrete
-    /// lists of requirements.
-    pub fn resolve(&self) -> Result<IndexMap<String, Vec<Requirement>>, Pep735Error> {
-        // Helper function to resolves a single group
-        fn resolve_single<'a>(
-            groups: &'a DependencyGroups,
-            group: &'a str,
-            resolved: &mut IndexMap<String, Vec<Requirement>>,
-            parents: &mut Vec<&'a str>,
-        ) -> Result<(), Pep735Error> {
-            let Some(specifiers) = groups.get(group) else {
-                // If the group included in another group does not exist, return an error
-                let parent = parents.iter().last().expect("should have a parent");
-                return Err(Pep735Error::GroupNotFound(
-                    group.to_string(),
-                    parent.to_string(),
-                ));
-            };
-            // If there is a cycle in dependency groups, return an error
-            if parents.contains(&group) {
-                return Err(Pep735Error::DependencyGroupCycle(Cycle(
-                    parents.iter().map(|s| s.to_string()).collect(),
-                )));
-            }
-            // If the dependency group has already been resolved, exit early
-            if resolved.get(group).is_some() {
-                return Ok(());
-            }
-            // Otherwise, perform recursion, as required, on the dependency group's specifiers
-            parents.push(group);
-            let mut requirements = Vec::with_capacity(specifiers.len());
-            for spec in specifiers.iter() {
-                match spec {
-                    // It's a requirement. Just add it to the Vec of resolved requirements
-                    DependencyGroupSpecifier::String(requirement) => {
-                        requirements.push(requirement.clone())
-                    }
-                    // It's a reference to another group. Recurse into it
-                    DependencyGroupSpecifier::Table { include_group } => {
-                        resolve_single(groups, include_group, resolved, parents)?;
-                        requirements
-                            .extend(resolved.get(include_group).into_iter().flatten().cloned());
-                    }
-                }
-            }
-            // Add the resolved group to IndexMap
-            resolved.insert(group.to_string(), requirements.clone());
-            parents.pop();
-            Ok(())
-        }
-
-        let mut resolved = IndexMap::new();
-        for group in self.keys() {
-            resolve_single(self, group, &mut resolved, &mut Vec::new())?;
-        }
-        Ok(resolved)
+    fn table_name() -> String {
+        "dependency-groups".to_string()
+    }
+    fn group_name() -> String {
+        "dependency group".to_string()
     }
 }
 
@@ -141,7 +71,7 @@ iota = [{include-group = "alpha"}]
         let dependency_groups = project_toml.dependency_groups.as_ref().unwrap();
         assert_eq!(
             dependency_groups.resolve().unwrap_err().to_string(),
-            String::from("Failed to find group `alpha` included by `iota`")
+            String::from("Failed to find dependency group `alpha` included by `iota`")
         )
     }
 }
